6d64718536cda15c1af3040a8b67918122012a26
naveenbusireddy <naveenbusireddy@gmail.com>
2021-09-14 16:38:29 +0530

    Feat: Translate feature added

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var txtInput = document.querySelector("#txt-input")
 var outputDiv = document.querySelector("#output")
 
 var serverURL = "https://api.funtranslations.com/translate/yoda.json"
+// var serverURL = "https://api.funtranslations.com/translate/groot.json"
 
 function getTranslationURL(input) {
     return serverURL + "?text=" + input;
@@ -19,7 +20,7 @@ function clickHandler() {
     var inputText = txtInput.value;
 
     fetch(getTranslationURL(inputText))
-        .then(response => response.json)
+        .then(response => response.json())
         .then(json => {
             var translatedText = json.contents.translated;
             outputDiv.innerText = translatedText;
